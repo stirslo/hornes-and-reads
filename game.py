2f09b71aede0d4af9c1585bb4a7534ae11618ada
Stirling Reynolds <stirlo@touch.asn.au>
2024-06-30 19:07:42 +1000

    refactor game for reuse

--- game.py
+++ game.py
@@ -2,74 +2,90 @@ import itertools
 
 X = "X"
 
-a1 = a2 = a3 = None
-b1 = b2 = b3 = None
-c1 = c2 = c3 = None
 
+class Game:
+    def __init__(self):
+        self.a1 = self.a2 = self.a3 = None
+        self.b1 = self.b2 = self.b3 = None
+        self.c1 = self.c2 = self.c3 = None
 
-def winner():
-    for player in [1, 2]:
-        if a1 == a2 == a3 == player:
+    def winner(self, player):
+        if self.a1 == self.a2 == self.a3 == player:
             return player
-        if b1 == b2 == b3 == player:
+        if self.b1 == self.b2 == self.b3 == player:
             return player
-        if c1 == c2 == c3 == player:
+        if self.c1 == self.c2 == self.c3 == player:
             return player
-        if a1 == b1 == c1 == player:
+        if self.a1 == self.b1 == self.c1 == player:
             return player
-        if a2 == b2 == c2 == player:
+        if self.a2 == self.b2 == self.c2 == player:
             return player
-        if a3 == b3 == c3 == player:
+        if self.a3 == self.b3 == self.c3 == player:
             return player
-        if a1 == b2 == c3 == player:
+        if self.a1 == self.b2 == self.c3 == player:
             return player
-        if c1 == b2 == a3 == player:
+        if self.c1 == self.b2 == self.a3 == player:
             return player
 
+    def make_move(self, player, move):
+        if self.a1 is None and move == "a1":
+            self.a1 = player
+        elif self.a2 is None and move == "a2":
+            self.a2 = player
+        elif self.a3 is None and move == "a3":
+            self.a3 = player
+        elif self.b1 is None and move == "b1":
+            self.b1 = player
+        elif self.b2 is None and move == "b2":
+            self.b2 = player
+        elif self.b3 is None and move == "b3":
+            self.b3 = player
+        elif self.c1 is None and move == "c1":
+            self.c1 = player
+        elif self.c2 is None and move == "c2":
+            self.c2 = player
+        elif self.c3 is None and move == "c3":
+            self.c3 = player
+        else:
+            print("invalid move")
+            return False
+        return True
 
-def make_move(player, move):
-    global a1, a2, a3, b1, b2, b3, c1, c2, c3
-    if a1 is None and move == "a1":
-        a1 = player
-    elif a2 is None and move == "a2":
-        a2 = player
-    elif a3 is None and move == "a3":
-        a3 = player
-    elif b1 is None and move == "b1":
-        b1 = player
-    elif b2 is None and move == "b2":
-        b2 = player
-    elif b3 is None and move == "b3":
-        b3 = player
-    elif c1 is None and move == "c1":
-        c1 = player
-    elif c2 is None and move == "c2":
-        c2 = player
-    elif c3 is None and move == "c3":
-        c3 = player
-    else:
-        print("invalid move")
-        return False
-    return True
+    def play(self):
 
+        for player in itertools.cycle([1, 2]):
+            while True:
+                move = input(f"player {player}? ")
+                if self.make_move(player, move):
+                    break
+            if self.winner(player):
+                print(f"player {player} wins!")
+                break
+            elif all(
+                [
+                    self.a1,
+                    self.a2,
+                    self.a3,
+                    self.b1,
+                    self.b2,
+                    self.b3,
+                    self.c1,
+                    self.c2,
+                    self.c3,
+                ]
+            ):
+                print("It's a draw")
+                break
 
-for player in itertools.cycle([1, 2]):
-    while True:
-        move = input(f"player {player}? ")
-        if make_move(player, move):
-            break
-    if winner():
-        print(f"player {player} wins!")
-        break
-    elif all([a1, a2, a3, b1, b2, b3, c1, c2, c3]):
-        print("It's a draw")
-        break
-
-
-print(
-    f"""
-{a1 or X} | {b1 or X} | {c1 or X}
-{a2 or X} | {b2 or X} | {c2 or X}
-{a3 or X} | {b3 or X} | {c3 or X}
+        print(
+            f"""
+{self.a1 or X} | {self.b1 or X} | {self.c1 or X}
+{self.a2 or X} | {self.b2 or X} | {self.c2 or X}
+{self.a3 or X} | {self.b3 or X} | {self.c3 or X}
 """
-)
+        )
+
+
+if __name__ == "__main__":
+    game = Game()
+    game.play()
